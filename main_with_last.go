6dbd4da432fb480dd888712d3bb3df14a735c8fb
Nikita <anisimoff.nikita@gmail.com>
2017-08-07 12:50:29 +0300

    init commit

---
+++ main_with_last.go
@@ -0,0 +1,25 @@
+package main
+
+import (  
+    "fmt"
+    "os"
+    "io/ioutil"
+    "strings"
+)
+
+func main() {  
+    if len(os.Args) != 2 {
+        fmt.Println("No input file")
+        return
+    } 
+
+    file, err := ioutil.ReadFile(os.Args[1]) 
+    if err != nil {
+        fmt.Print(err)
+    }
+
+    s := string(file)
+    n := strings.Count(s, "\n") + 1
+    
+    fmt.Println(n)
+}
